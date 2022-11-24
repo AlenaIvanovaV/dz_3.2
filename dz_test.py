04a0cd21af48b6d92c97129a0d122d843aa67875
ivanova_av10 <angel40995@inbox.ru>
2022-11-24 22:21:42 +0300

    test2

--- dz_test.py
+++ dz_test.py
@@ -5,3 +5,8 @@ from selene.support.shared import browser
 def test_search(open_browser):
     browser.element('[name=q]').type('selene').press_enter()
     browser.element('[id=search]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
+
+def test_bad(open_browser):
+    browser.element('[name=q]').type('jhnmtnmjhx jmhrth').press_enter()
+    browser.element('[id=result-stats]').should(have.text('Результатов: примерно 0'))
